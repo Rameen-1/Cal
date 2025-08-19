602897666816cb8de5f0effc2b5c3bf5b53fb4e1
Rameen Zeeshan <rameen.zeeshanedu@gmail.com>
2025-08-19 16:45:28 +0400

    Create header.php

---
+++ header.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>CandyCal</title>
+  <link rel="stylesheet" href="style.css">
+  <link href="https://fonts.googleapis.com/css2?family=Nunito&family=Poppins:wght@300;400&family=Quicksand&family=Raleway:wght@200;400&display=swap" rel="stylesheet">
+</head>
+<body>
